a3d660b2cc0f068a96237a6d4e86ba6d474c4788
Nokrux <julien.deslandes@protonmail.com>
2022-11-30 21:46:08 +0100

    [ADD] Redis et nextcloud cron

--- dev-nextcloud-pod.sh
+++ dev-nextcloud-pod.sh
@@ -9,6 +9,8 @@ mkdir -p $DB_DIR
 DB_CONTAINER=db
 NEXTCLOUD_CONTAINER=app
 WEB_SERVER_CONTAINER=web
+REDIS_CONTAINER=redis
+CRON_CONTAINER=cron
 
 # Pod name
 POD=nextcloud-pod
@@ -21,12 +23,13 @@ if podman pod ls | grep -q $POD; then
 	podman pod rm -f $POD
 fi
 
+HOST=127.0.0.1
+
 # Db Env vars
 DB_USER=nextcloud
 DB_PWD=password
 DB_ROOT_PWD=root-pwd
 DB_NAME=db
-DB_HOST=127.0.0.1
 
 # Nextcloud Env vars
 NEXTCLOUD_ADMIN_USER=admin   \
@@ -51,19 +54,35 @@ podman run -d \
     mariadb:10.9 \
 	--transaction-isolation=READ-COMMITTED --binlog-format=ROW
 
+# Starts redis container
+podman run -d \
+    --name $REDIS_CONTAINER \
+	--pod $POD \
+    redis:7.0-alpine
+
 # Starts Nextcloud container
 podman run -d \
 	--name $NEXTCLOUD_CONTAINER \
 	--pod $POD \
-	--requires $DB_CONTAINER \
+	--requires $DB_CONTAINER,$REDIS_CONTAINER \
 	-e MYSQL_USER=$DB_USER \
     -e MYSQL_PASSWORD=$DB_PWD \
 	-e MYSQL_ROOT_PASSWORD=$DB_ROOT_PWD \
     -e MYSQL_DATABASE=$DB_NAME \
-    -e MYSQL_HOST=$DB_HOST \
+    -e MYSQL_HOST=$HOST \
+    -e REDIS_HOST=$HOST \
     -e NEXTCLOUD_ADMIN_USER=$NEXTCLOUD_ADMIN_USER \
     -e NEXTCLOUD_ADMIN_PASSWORD=$NEXTCLOUD_ADMIN_PASSWORD \
     -e NEXTCLOUD_TRUSTED_DOMAINS=$NEXTCLOUD_TRUSTED_DOMAINS \
+	-v "$PWD"/$NEXTCLOUD_DIR:/var/www/html \
+	nextcloud:23-fpm-alpine
+
+# Starts Nextcloud Cron container
+podman run -d \
+	--name $CRON_CONTAINER \
+	--pod $POD \
+	--requires $DB_CONTAINER,$REDIS_CONTAINER \
+	--entrypoint /cron.sh \
 	-v "$PWD"/$NEXTCLOUD_DIR:/var/www/html \
 	nextcloud:23-fpm-alpine
 
@@ -77,9 +96,11 @@ podman run -d \
 podman run -d \
 	--name $WEB_SERVER_CONTAINER \
 	--pod $POD \
+	--requires $NEXTCLOUD_CONTAINER \
 	-v "$PWD"/web/dev/Caddyfile:/etc/caddy/Caddyfile \
 	-v caddy_data:/data \
 	--volumes-from app \
 	caddy:2.6-alpine
 
 # 	-v "$PWD"/$NEXTCLOUD_DIR:/var/www/html:z,ro \
+echo "Nextcloud now running at 127.0.0.1:8080"
