5f307af170135f562de4925482f673ceb9682a29
Nokrux <julien.deslandes@protonmail.com>
2022-12-06 16:13:45 +0100

    Update caddy-reload

--- caddy_reload.sh
+++ caddy_reload.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-caddy_container_id=$(podman ps | grep caddy | awk '{print $1;}')
-echo "Caddy container id: $caddy_container_id"
-podman exec -w /etc/caddy "$caddy_container_id" caddy reload
+podman exec -w /etc/caddy web caddy reload
 echo "Caddy reloaded"
